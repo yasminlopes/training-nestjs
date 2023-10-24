74054917b101b506ae55bc33fe0441b48b43ed22
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 09:39:49 -0300

    feat: create endpoints

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  public async index() {
+    return await this.todoService.findAll();
+  }
+
+  @Get(':id')
+  public async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoService.findOneOrFail(id);
+  }
+
+  @Post()
+  public async create(@Body() body) {
+    return await this.todoService.create(body);
+  }
+
+  @Put(':id')
+  public async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body,
+  ) {
+    return await this.todoService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteById(id);
+  }
+}
