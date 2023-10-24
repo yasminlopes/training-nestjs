8113ffa3b2c929b442f2b9e2144895eb6097c51b
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 09:20:36 -0300

    feat: create todo entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { TodoModule } from './app/todo/todo.module';
         username: configService.get('DB_USERNAME', 'root'),
         password: configService.get('DB_PASSWORD', '123'),
         database: configService.get('DB_DATABASE', 'todo'),
-        entities: [],
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
     }),
