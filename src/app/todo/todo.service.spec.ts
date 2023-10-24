4d0871111217e1d84d3884b66993309b1ffba08f
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 14:54:50 -0300

    test: add tests e2e for todo service

--- src/app/todo/todo.service.spec.ts
+++ src/app/todo/todo.service.spec.ts
@@ -1,18 +1,207 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoEntity } from './entity/todo.entity';
 import { TodoService } from './todo.service';
 
+const todoEntityList: TodoEntity[] = [
+  new TodoEntity({ task: 'task-1', isDone: 0 }),
+  new TodoEntity({ task: 'task-2', isDone: 0 }),
+  new TodoEntity({ task: 'task-3', isDone: 0 }),
+];
+
+const updatedTodoEntityItem = new TodoEntity({ task: 'task-1', isDone: 1 });
+
 describe('TodoService', () => {
-  let service: TodoService;
+  let todoService: TodoService;
+  let todoRepository: Repository<TodoEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getRepositoryToken(TodoEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue(todoEntityList),
+            findOneOrFail: jest.fn().mockResolvedValue(todoEntityList[0]),
+            create: jest.fn().mockReturnValue(todoEntityList[0]),
+            merge: jest.fn().mockReturnValue(updatedTodoEntityItem),
+            save: jest.fn().mockResolvedValue(todoEntityList[0]),
+            softDelete: jest.fn().mockReturnValue(undefined),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    todoService = module.get<TodoService>(TodoService);
+    todoRepository = module.get<Repository<TodoEntity>>(
+      getRepositoryToken(TodoEntity),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(todoService).toBeDefined();
+    expect(todoRepository).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a todo entity list successfully', async () => {
+      // act
+      const result = await todoService.findAll();
+
+      // assert
+      expect(result).toEqual(todoEntityList);
+      expect(todoRepository.find).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      // arrange
+      jest.spyOn(todoRepository, 'find').mockRejectedValueOnce(new Error());
+
+      // assert
+      expect(todoService.findAll()).rejects.toThrowError();
+    });
+  });
+
+  describe('findOneOrFail', () => {
+    it('should return a todo entity item successfully', async () => {
+      // act
+      const result = await todoService.findOneOrFail('1');
+
+      // assert
+      expect(result).toEqual(todoEntityList[0]);
+      expect(todoRepository.findOneOrFail).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a not found exception', () => {
+      // arrange
+      jest
+        .spyOn(todoRepository, 'findOneOrFail')
+        .mockRejectedValueOnce(new Error());
+
+      // assert
+      expect(todoService.findOneOrFail('1')).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new todo entity item successfully', async () => {
+      // arrange
+      const data: CreateTodoDto = {
+        task: 'task-1',
+        isDone: 0,
+      };
+
+      // act
+      const result = await todoService.create(data);
+
+      // assert
+      expect(result).toEqual(todoEntityList[0]);
+      expect(todoRepository.create).toHaveBeenCalledTimes(1);
+      expect(todoRepository.save).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      // arrange
+      const data: CreateTodoDto = {
+        task: 'task-1',
+        isDone: 0,
+      };
+
+      jest.spyOn(todoRepository, 'save').mockRejectedValueOnce(new Error());
+
+      // assert
+      expect(todoService.create(data)).rejects.toThrowError();
+    });
+  });
+
+  describe('update', () => {
+    it('should update a todo entity item successfully', async () => {
+      // arrange
+      const data: UpdateTodoDto = {
+        task: 'task-1',
+        isDone: 1,
+      };
+
+      jest
+        .spyOn(todoRepository, 'save')
+        .mockResolvedValueOnce(updatedTodoEntityItem);
+
+      // act
+      const result = await todoService.update('1', data);
+
+      // assert
+      expect(result).toEqual(updatedTodoEntityItem);
+    });
+
+    it('should throw a not found exception', () => {
+      // arrange
+      jest
+        .spyOn(todoRepository, 'findOneOrFail')
+        .mockRejectedValueOnce(new Error());
+
+      const data: UpdateTodoDto = {
+        task: 'task-1',
+        isDone: 1,
+      };
+
+      // assert
+      expect(todoService.update('1', data)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+
+    it('should throw an exception', () => {
+      // arrange
+      jest.spyOn(todoRepository, 'save').mockRejectedValueOnce(new Error());
+
+      const data: UpdateTodoDto = {
+        task: 'task-1',
+        isDone: 1,
+      };
+
+      // assert
+      expect(todoService.update('1', data)).rejects.toThrowError();
+    });
+  });
+
+  describe('deleteById', () => {
+    it('should delete a todo entity item successfully', async () => {
+      // act
+      const result = await todoService.deleteById('1');
+
+      // assert
+      expect(result).toBeUndefined();
+      expect(todoRepository.findOneOrFail).toHaveBeenCalledTimes(1);
+      expect(todoRepository.softDelete).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a not found exception', () => {
+      // arrange
+      jest
+        .spyOn(todoRepository, 'findOneOrFail')
+        .mockRejectedValueOnce(new Error());
+
+      // assert
+      expect(todoService.deleteById('1')).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+
+    it('should throw an exception', () => {
+      // arrange
+      jest
+        .spyOn(todoRepository, 'softDelete')
+        .mockRejectedValueOnce(new Error());
+
+      // assert
+      expect(todoService.deleteById('1')).rejects.toThrowError();
+    });
   });
 });
