ab5f566d1bc1814252bef0a1d701e3d1a7eefece
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 17:16:44 -0300

    feat: implement users service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 import { UsersEntity } from './entity/users.entity';
-
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: Repository<UsersEntity>) {}
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  public async findAll() {
+    return await this.usersRepository.find({
+      select: ['id', 'firstName', 'lastName', 'email'],
+    });
+  }
+
+  public async findOneOrFail(options?: FindOneOptions<UsersEntity>) {
+    try {
+      return await this.usersRepository.findOneOrFail(options);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  public async store(user: CreateUserDto) {
+    return await this.usersRepository.save(this.usersRepository.create(user));
+  }
+
+  public async update(id: string, user: UpdateUserDto) {
+    const userFound = await this.findOneOrFail({ where: { id } });
+    this.usersRepository.merge(userFound, user);
+    return await this.usersRepository.save(userFound);
+  }
+
+  public async destroy(id: string) {
+    await this.findOneOrFail({ where: { id } });
+
+    await this.usersRepository.softDelete(id);
+  }
 }
