64467e13ba83d1d0cf036ec568d61060676472a9
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 09:39:36 -0300

    feat: create crud functions service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  public async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  public async findOneOrFail(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  public async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  public async update(id: string, data) {
+    const todo = await this.findOneOrFail(id);
+
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  public async deleteById(id: string) {
+    await this.findOneOrFail(id);
+
+    await this.todoRepository.softDelete(id);
+  }
+}
