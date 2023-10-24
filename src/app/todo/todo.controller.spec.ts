64402d9804f0876a8a3d38d1ec38e80d5ac29093
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 11:40:25 -0300

    test: ensure TodoService to be defined

--- src/app/todo/todo.controller.spec.ts
+++ src/app/todo/todo.controller.spec.ts
@@ -4,6 +4,7 @@ import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let todoController: TodoController;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,9 +24,11 @@ describe('TodoController', () => {
     }).compile();
 
     todoController = module.get<TodoController>(TodoController);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
     expect(todoController).toBeDefined();
+    expect(todoService).toBeDefined();
   });
 });
