3f78d7a4333a987008c6b8cdfb94fb0fac49660d
Yasmin Lopes <yclopes27@gmail.com>
2023-10-24 09:05:24 -0300

    build: configure typeorm and envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3307)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '123'),
+        database: configService.get('DB_DATABASE', 'todo'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
