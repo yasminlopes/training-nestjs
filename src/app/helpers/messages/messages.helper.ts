33834b9206f9a4f8c0886c4a5590c2b65d7c1811
Yasmin Lopes <yclopes27@gmail.com>
2023-10-25 08:49:41 -0300

    feat: add password or email invalid message helper

--- src/app/helpers/messages/messages.helper.ts
+++ src/app/helpers/messages/messages.helper.ts
@@ -1,3 +1,4 @@
 export const messagesHelper = {
   PASSWORD_VALID: `A senha deve conter letras maiúsculas, minusculas, números e caracteres especiais.`,
+  PASSWORD_OR_EMAIL_INVALID: `E-mail e/ou senha inválidos.`,
 };
